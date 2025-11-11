c0a2b0e9899181f271028e66ad6621a34d748985
akhilag2 <ageorg5@gmu.edu>
2025-11-11 15:16:12 +0530

    Modified webpage

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ def index():
 @app.route('/predictdata',methods=['GET','POST'])
 def predict_datapoint():
     if request.method=='GET':
-        return render_template('home.html')
+        return render_template('home.html', results=None)
     else:
         data=CustomData(
             gender=request.form.get('gender'),
